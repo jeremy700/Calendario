155a81ff6b4253e329f722e59fd5612789a93475
jeremy <jeremy@kinetos.pc-jeremy>
2018-06-28 15:19:47 -0600

    Se agrega funcionalidad para que el calendario inicie el dia indicado

--- componenteCalendarios/calendarios.controller.js
+++ componenteCalendarios/calendarios.controller.js
@@ -4,17 +4,26 @@ app.controller('calendariosCtrl', function($scope){
     $scope.calendarios.cantidadDias='';
     //$scope.calendarios.vectorDias2=[1,2,3,4,5,6,7,8,9,10,11,12];
 
-    $scope.calendarios.crearElemento=function(fecha){
+    $scope.calendarios.crearDia=function(fecha){
         var elemento={}
-        var arrayFecha=fecha.toString().split(' ');
-        elemento.dia=arrayFecha[0];
-        elemento.mes=arrayFecha[1];
-        elemento.numDia=arrayFecha[2];
-        elemento.ano=arrayFecha[3];
-        return elemento;
+        if(fecha!=""){
+            var arrayFecha=fecha.toString().split(' ');
+            elemento.dia=arrayFecha[0];
+            elemento.mes=arrayFecha[1];
+            elemento.numDia=arrayFecha[2];
+            elemento.ano=arrayFecha[3];
+            return elemento;
+        }else{
+            elemento.dia='';
+            elemento.mes='';
+            elemento.numDia='';
+            elemento.ano='';
+            return elemento;
+        }
+        
     }
 
-    $scope.calendarios.matrixList = function(data, n) {
+    $scope.calendarios.crearMatriz = function(data, n) {
         var grid = [], i = 0, x = data.length, col, row = -1;
         for (var i = 0; i < x; i++) {
             col = i % n;
@@ -29,19 +38,38 @@ app.controller('calendariosCtrl', function($scope){
     $scope.calendarios.generarCalendarios=function(){
         if(($scope.calendarios.fechaInicial!='')&&($scope.calendarios.cantidadDias!='')){
             $scope.calendarios.vectorDias=[];
-            var fecha = $scope.calendarios.fechaInicial;
-            $scope.calendarios.vectorDias.push($scope.calendarios.crearElemento(fecha));
+            var fechaInicial=$("#start").val();
+            var arrayAordenar=fechaInicial.split('-');
+            var fechaEspanol=arrayAordenar[0]+"/"+arrayAordenar[1]+"/"+arrayAordenar[2];
+            
+            var primerDia=new Date(arrayAordenar[0]+"/"+arrayAordenar[1]+"/01");
+            var primerDiaNum=primerDia.getDay();
+            var fecha = new Date(fechaEspanol);
+            
+            if(primerDia.getTime()!=fecha.getTime()){
+                console.log(fecha.getDate());
+                primerDiaNum=primerDiaNum+(fecha.getDate()-1);
+            }
+
+            $scope.calendarios.vectorDias.push($scope.calendarios.crearDia(fecha));
             for(var i=1; i<$scope.calendarios.cantidadDias; i++){
                 fecha.setDate(fecha.getDate() + 1);
-                $scope.calendarios.vectorDias.push($scope.calendarios.crearElemento(fecha));
-
+                $scope.calendarios.vectorDias.push($scope.calendarios.crearDia(fecha));
+                
                 if(i>=$scope.calendarios.cantidadDias-1){
-                    console.log($scope.calendarios.vectorDias);   
-                    $scope.calendarios.inspireList = $scope.calendarios.matrixList($scope.calendarios.vectorDias, 7);
-                    console.log($scope.calendarios.inspireList);   
+                    if(primerDiaNum==0){
+                        $scope.calendarios.inspireList = $scope.calendarios.crearMatriz($scope.calendarios.vectorDias, 7);    
+                    }else{
+                        for(var j=0; j<primerDiaNum; j++){
+                            $scope.calendarios.vectorDias.unshift($scope.calendarios.crearDia(''));
+                            if(j>=primerDiaNum-1){
+                                $scope.calendarios.inspireList = $scope.calendarios.crearMatriz($scope.calendarios.vectorDias, 7);
+                            }
+                        }
+                    }
                 }
             }
+            
         }
     }
-    //http://techbloghunting.com/2016/11/08/angularjs-ng-repeat-array-rows-columns/
 });
